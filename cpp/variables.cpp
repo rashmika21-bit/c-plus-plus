c0315ba1cf6da509cd33625a4af6e5d195008fa6
rashmika21-bit <ranarashmika21@gmail.com>
2025-06-14 23:21:34 +0530

    Initial commit

---
+++ cpp/variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int x; //declaration of variable x
+    // x = 5; //initialization of variable x
+    // //x = 10; //re-initialization of variable x 
+    // cout << x+5; //this will output 10
+
+    int x=5, y=9;
+    cout<< x+y;
+    return 0;
+} 
