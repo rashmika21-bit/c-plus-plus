f9d4737e6f2ceb192e240dde5ccd2685813b60ab
Rashmika <ranarashmika21@gmail.com>
2025-06-22 09:33:03 +0530

    Delete characterdatatype.cpp

--- characterdatatype.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    char ch;
-    cout << "Enter a character: ";
-    cin>> ch;
-    cout << "You entered: " << ch << endl;
-    cout << "ASCII value of '" << ch << "' is: " << int(ch) << endl;abort
-}
